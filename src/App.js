4ec0f866c630e8521de13edde8343a3ce6385726
Loi Tran <datloiboi@gmail.com>
2020-04-01 14:51:56 +0700

    Add PWA

--- src/App.js
+++ src/App.js
@@ -4,68 +4,30 @@ import "./App.css";
 import ChoiceCard from "./components/ChoiceCard";
 import ChoiceButtons from "./components/ChoiceButtons";
 
-const CHOICES = {
-  scissors: {
-    name: "scissors",
-    url: "http://www.pngmart.com/files/1/Scissors-PNG-Pic.png"
-  },
-  paper: {
-    name: "paper",
-    url: "http://pngimagesfree.com/Paper/Thumb/blank-note-paper-free-clipa.png"
-  },
-  rock: {
-    name: "rock",
-    url:
-      "https://opengameart.org/sites/default/files/forum-attachments/very%20simple%20rock_0.png"
-  }
-};
-
-const getRandomChoice = () => {
-  var keys = Object.keys(CHOICES);
-  return CHOICES[keys[(keys.length * Math.random()) << 0]];
-};
-
-const getRoundOutcome = userChoice => {
-  const computerChoice = getRandomChoice().name;
-  let result;
-
-  if (userChoice === "rock") {
-    result = computerChoice === "scissors" ? "Victory!" : "Defeat!";
-  }
-  if (userChoice === "paper") {
-    result = computerChoice === "rock" ? "Victory!" : "Defeat!";
-  }
-  if (userChoice === "scissors") {
-    result = computerChoice === "paper" ? "Victory!" : "Defeat!";
-  }
-
-  if (userChoice === computerChoice) result = "Tie game!";
-  return [result, computerChoice];
-};
+import { CHOICES, getRoundOutcome } from "./utils";
 
 function App() {
-  const [computerChoice, setComputerChoice] = useState(getRandomChoice());
-  const [playerChoice, setPlayerChoice] = useState(null);
+  const [prompt, setGamePrompt] = useState("1, 2, 3, SHOOT!");
   const [gameHistory, setGameHistory] = useState([]);
-  const [prompt, setGamePrompt] = useState(null);
   const [previousWinner, setPreviousWinner] = useState(null);
+  const [playerChoice, setPlayerChoice] = useState(null);
+  const [computerChoice, setComputerChoice] = useState(null);
 
   const onPlayerChoose = playerChoice => {
     const [result, compChoice] = getRoundOutcome(playerChoice);
+
     const newUserChoice = CHOICES[playerChoice];
     const newComputerChoice = CHOICES[compChoice];
+
     setGamePrompt(result);
     gameHistory.push(result);
     setGameHistory(gameHistory);
     setPlayerChoice(newUserChoice);
     setComputerChoice(newComputerChoice);
-    if (result === "Victory!") {
-      setPreviousWinner("You");
-    } else if (result === "Defeat!") {
-      setPreviousWinner("Computer");
-    } else {
-      setPreviousWinner("Tie");
-    }
+
+    if (result === "Victory!") setPreviousWinner("You");
+    else if (result === "Defeat!") setPreviousWinner("Computer");
+    else setPreviousWinner("Tie");
   };
 
   return (
@@ -75,7 +37,7 @@ function App() {
           <div className="col-md-8 themed-grid-col">
             <ChoiceCard
               title="Computer"
-              imgURL={computerChoice.url}
+              imgURL={computerChoice && computerChoice.url}
               previousWinner={previousWinner}
             />
             <h1>{prompt}</h1>
